3e84f7d81b4402d7da3291fc22b5ca763f799808
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-08 09:02:07 +0000

    Deployed wftm-beftm

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0xF242E207f13c09d2f59cC185e50479BB5e4E27c3';
+  const strategyAddress = '0x796a2166be078D8AebBDf2ccBA8C2C2385adA833';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
