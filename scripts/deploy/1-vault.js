4f00042ca6b5b964c867d624430029208835e569
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-09 23:00:18 +0100

    Deployed wftm-solidsex

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,14 +1,14 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
 
-  const ftmTombLPAddress = '0x60a861Cd30778678E3d613db96139440Bd333143';
-  const wantAddress = ftmTombLPAddress;
-  const tokenName = 'Solidex WFTM-TOMB Crypt';
-  const tokenSymbol = 'rfvAMM-WFTM-TOMB';
+  const wantAddress = '0xa66901D1965F5410dEeB4d0Bb43f7c1B628Cb20b';
+  const tokenName = 'WFTM-SOLIDSEX Solidex Crypt';
+  const tokenSymbol = 'rfvAMM-WFTM-SOLIDSEX';
   const depositFee = 0;
   const tvlCap = ethers.utils.parseEther('2000');
+  const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
 
-  const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap);
+  const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap, options);
 
   await vault.deployed();
   console.log('Vault deployed to:', vault.address);
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,12 +1,12 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0x7FE0286fb990c101E7a6727Dd72Bd57B9BfCcc33';
+  const strategyAddress = '0xaAB8369086AFC5B8Dc5152fd099138c8bac2a86A';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
 
-  //const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
-  await vault.initialize(strategyAddress);
+  const options = { gasPrice: 500000000000, gasLimit: 9000000 };
+  await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
 }
 
