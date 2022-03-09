cf6bc2c82cf765c800fcb182bce92d6cdd716c3d
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-09 18:12:06 +0100

    Deployed wftm-sex

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,12 +1,12 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0xb5c492944f08C64974568A06271910880C4108C6';
+  const strategyAddress = '0xfc118182065FE337C56bA8145947A8D772d0a477';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
 
-  //const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
-  await vault.initialize(strategyAddress);
+  const options = { gasPrice: 400000000000, gasLimit: 9000000 };
+  await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
 }
 
