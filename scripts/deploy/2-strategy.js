72a734f02da630026d972e2b20d9820b1c8bdf54
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-14 11:28:26 +0100

    Deployed usdc-dei

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function main() {
-  const vaultAddress = '0x3DDA58660f658Efe48895F57e7Dc4bBF462557D9';
+  const vaultAddress = '0x33462eae91e9dEB43a9aF50f3A5ae50E7d46FC69';
 
   const Strategy = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
   const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
@@ -9,12 +9,12 @@ async function main() {
   const strategist1 = '0x1E71AEE6081f62053123140aacC7a06021D77348';
   const strategist2 = '0x81876677843D00a7D792E1617459aC2E93202576';
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
-  const wantAddress = '0xD9A4108CBB40a12dE16dFFDc54aE5065878816d7';
+  const wantAddress = '0x5821573d8F04947952e76d94f3ABC6d7b43bF8d0';
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
     [vaultAddress, [treasuryAddress, paymentSplitterAddress], [strategist1, strategist2, strategist3], wantAddress],
-    { kind: 'uups', timeout: 0, gasPrice: 2400000000000, gasLimit: 9000000 },
+    { kind: 'uups', timeout: 0, gasPrice: 300000000000, gasLimit: 9000000 },
   );
   await strategy.deployed();
   console.log('Strategy deployed to:', strategy.address);
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,11 +1,11 @@
 async function main() {
-  const vaultAddress = '0x3DDA58660f658Efe48895F57e7Dc4bBF462557D9';
-  const strategyAddress = '0x9b80A745e01989A49024B8EA754a57c1b374a279';
+  const vaultAddress = '0x33462eae91e9dEB43a9aF50f3A5ae50E7d46FC69';
+  const strategyAddress = '0x5CcEdedb013A37fAE9dC332130396eEA80f3165d';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
 
-  const options = { gasPrice: 1000000000000, gasLimit: 9000000 };
+  const options = { gasPrice: 300000000000, gasLimit: 9000000 };
   await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
 }
